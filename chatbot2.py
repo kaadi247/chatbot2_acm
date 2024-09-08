be841d256db3336eafa560571748c93a7a055b55
kaadi247 <143442816+kaadi247@users.noreply.github.com>
2024-09-09 00:47:01 +0530

    Update chatbot2.py

--- chatbot2.py
+++ chatbot2.py
@@ -1,29 +1,30 @@
-import streamlit as st
-from langchain import HuggingFaceHub
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
+import streamlit as st # allows us to use streamlit
+from langchain import HuggingFaceHub # allows us to use huggingface and call api mode
+from langchain.chains import LLMChain # allows us to use LLMChain for output
+from langchain.prompts import PromptTemplate # allows us to give prompt to system according to syntax
 
-st.title("Hugging Face Chatbot")
+st.title("Hugging Face Chatbot") # title on streamlit
 
-HUGGINGFACE_API_KEY = "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"
+HUGGINGFACE_API_KEY = "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" #api key from huggingface
 
-model_name = "mistralai/Mistral-7B-Instruct-v0.3"
+model_name = "mistralai/Mistral-7B-Instruct-v0.3" #ai model used
 
-prompt_template = """
+#gives prompt to system
+prompt_template = """ 
 You are polite, helpful assistant. Respond to the users queries while holding context across messages
 Human: {input}
 System:
 """
 
-template = PromptTemplate(input_variables=["input"], template=prompt_template)
+template = PromptTemplate(input_variables=["input"], template=prompt_template) #creates final prompt template 
 
-llm = HuggingFaceHub(repo_id=model_name, huggingfacehub_api_token=HUGGINGFACE_API_KEY)
+llm = HuggingFaceHub(repo_id=model_name, huggingfacehub_api_token=HUGGINGFACE_API_KEY) #creates llm using HuggingFaceHub
 
-llm_chain = LLMChain(prompt=template, llm=llm)
+llm_chain = LLMChain(prompt=template, llm=llm) # creates llm_chain using LLMChain to produce output
 
-st.write("Ask me a question")
-user_input = st.text_input("You: ", placeholder="Ask me a question")
+st.write("Ask me a question") # message displayed on streamlit
+user_input = st.text_input("You: ", placeholder="Ask me a question") #message displayed on searchbar and takes input
 
-if user_input:
+if user_input: # runs the code and gets response using the AI model
     response = llm_chain.run(user_input)
     st.write(f"System: {response}")
